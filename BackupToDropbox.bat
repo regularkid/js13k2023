72439a2d4463f94c93ad53da6def70d8f70e2568
Bryan Perfetto <regularkid@gmail.com>
2023-08-07 19:45:47 -0700

    Now using %UserProfile% var instead of hard coding Dropbox path.

--- BackupToDropbox.bat
+++ BackupToDropbox.bat
@@ -7,14 +7,15 @@ SET day=%DATE:~7,2%
 SET hour=%TIME:~0,2%
 SET minute=%TIME:~3,2%
 SET second=%TIME:~6,2%
-SET backupPath=C:\Users\regul\Dropbox\SuperMicroFun\Projects\js13k_2023_Backups\Backup_%year%_%month%_%day%__%hour%_%minute%_%second%.zip
+SET backupDirectory=%UserProfile%\Dropbox\SuperMicroFun\Projects\js13k_2023_Backups
+SET backupPath=%backupDirectory%\Backup_%year%_%month%_%day%__%hour%_%minute%_%second%.zip
 SET devPath=.\*
 
 REM Zip up entire dev folder and put it in Dropbox
 CALL "C:\Program Files\7-Zip\7z.exe" a -tzip %backupPath% %devPath%
 
 REM Display to verify that it worked
-DIR /a-d /o-d C:\Users\regul\Dropbox\SuperMicroFun\Projects\js13k_2023_Backups\
+DIR /a-d /o-d %backupDirectory%
 
 REM Wait for user input
 PAUSE
